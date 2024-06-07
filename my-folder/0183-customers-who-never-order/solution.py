85374c94db7a96dccdef56e58bb04efc665ee787
Priyank Gupta <71163480+priyankgupta2003@users.noreply.github.com>
2024-06-07 06:12:07 +0000

    [LeetCode Sync] Runtime - 423 ms (48.36%), Memory - 67.1 MB (39.97%)

---
+++ my-folder/0183-customers-who-never-order/solution.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers.merge(orders, left_on='id', right_on='customerId', how='left')
+    df = df[df['customerId'].isna()]
+    df = df[['name']].rename(columns={'name': 'Customers'})
+    return df
+    
